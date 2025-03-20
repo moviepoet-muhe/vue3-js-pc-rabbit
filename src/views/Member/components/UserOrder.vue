7a983929c12951e313be056a1476e0c529c3f33a
moviepoet-muhe <dengweicnm@gmail.com>
2025-03-20 22:46:24 +0800

    订单-使用tap切换获取状态数据

--- src/views/Member/components/UserOrder.vue
+++ src/views/Member/components/UserOrder.vue
@@ -28,11 +28,18 @@ const getOrderList = async () => {
 
 getOrderList()
 
+// tab切换
+const tabChange = (type) => {
+  // 使用当前的状态数据发送请求
+  params.value.orderState = type
+  getOrderList()
+}
+
 </script>
 
 <template>
   <div class="order-container">
-    <el-tabs>
+    <el-tabs @tab-change="tabChange">
       <!-- tab切换 -->
       <el-tab-pane v-for="item in tabTypes" :key="item.name" :label="item.label" />
 
